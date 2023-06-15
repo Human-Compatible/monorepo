9a55f5cf6f93ad340b9bba1b6d3ae789a3473d00
Simon Biggs <me@simonbiggs.net>
2023-06-15 11:30:06 +1000

    update traits

--- workspaces/assistance/assistance/_faq/answer.py
+++ workspaces/assistance/assistance/_faq/answer.py
@@ -65,6 +65,12 @@ PROMPT = textwrap.dedent(
         - You are trying to find ways to help them be successful in
           their application
         - You are helpful and friendly
+        - You are ensuring they are aware of all strict requirements,
+          especially being a South African Citizen, being able to prove
+          access to funds, and having the ability to pass an IELTS test
+          or have an exemption.
+        - You ask clarifying questions when applicable, especially when
+          it is unclear if they meet the strict requirements
 
         ## Question asked by THIS applicant
 
